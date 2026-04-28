50d33bb490754e07c6015ac84839b5b66b03544d
yusupovairoda915-cyber <yusupovairoda915@gmail.com>
2026-04-28 15:08:03 +0500

    config: добавить ID администратора

--- config.py
+++ config.py
@@ -12,8 +12,10 @@ class Config:
     DB_URL = os.getenv("DB_URL")
     
     # Путь к твоему JSON-файлу с ключами (паспорт Firebase)
-    FIREBASE_KEY_PATH = "serviceAccountKey.json" 
+    FIREBASE_KEY_PATH = "serviceAccountKey.json"
+    
+    # ID администратора (замени на ID @prosto_993)
+    ADMIN_ID = 123456789  # ЗАМЕНИ НА РЕАЛЬНЫЙ ID!
 
 # Экземпляр конфига для удобного импорта
 config = Config()
-
