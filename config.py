67ca74095c6f3c1feb817ebb89b9bb5fa67445aa
yusupovairoda915-cyber <yusupovairoda915@gmail.com>
2026-04-28 15:19:24 +0500

    Update ADMIN_ID to a specific value

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     FIREBASE_KEY_PATH = "serviceAccountKey.json"
     
     # ID администратора (замени на ID @prosto_993)
-    ADMIN_ID = os.getenv("ADMIN_ID")  # ЗАМЕНИ НА РЕАЛЬНЫЙ ID!
+    ADMIN_ID = 6924098464  # ЗАМЕНИ НА РЕАЛЬНЫЙ ID!
 
 # Экземпляр конфига для удобного импорта
 config = Config()
